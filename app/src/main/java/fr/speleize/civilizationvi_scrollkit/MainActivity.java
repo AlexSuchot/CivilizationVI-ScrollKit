1ceb2460af428caa7707d01dc6940ee4e46b643c
Unknown <alexandre.suchot@ynov.com>
2019-01-09 12:06:08 +0100

    commit pre tacos

--- app/src/main/java/fr/speleize/civilizationvi_scrollkit/MainActivity.java
+++ app/src/main/java/fr/speleize/civilizationvi_scrollkit/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.speleize.civilizationvi_scrollkit;
 
+        import android.content.Intent;
+        import android.graphics.Typeface;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Font pour le titre :
+        Typeface PermanentMarkerRegular = Typeface.createFromAsset(getAssets(),
+                "font/penumbra.ttf");
+        TextView textView = findViewById(R.id.title);
+        textView.setTypeface(PermanentMarkerRegular);
+    }
+
+    public static void goToCivilizations(){
+        Intent intent = new Intent(this, );
+
+
+    }
+
+    public static void goToBuildings(){
+
+    }
+
+    public static void goToUnits(){
+
     }
 }
