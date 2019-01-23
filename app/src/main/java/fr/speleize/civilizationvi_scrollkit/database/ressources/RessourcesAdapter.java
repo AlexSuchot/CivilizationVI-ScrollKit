cd43f3db6631e79853d55d60ae72276d01200feb
Speleize <alexandre.suchot@ynov.com>
2019-01-23 20:25:43 +0100

    add linear layout to ressources

--- app/src/main/java/fr/speleize/civilizationvi_scrollkit/database/ressources/RessourcesAdapter.java
+++ app/src/main/java/fr/speleize/civilizationvi_scrollkit/database/ressources/RessourcesAdapter.java
@@ -1,18 +1,28 @@
 package fr.speleize.civilizationvi_scrollkit.database.ressources;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
 import fr.speleize.civilizationvi_scrollkit.R;
 import fr.speleize.civilizationvi_scrollkit.classes.Ressource;
 
+import static android.app.PendingIntent.getActivity;
+
 public class RessourcesAdapter extends RecyclerView.Adapter<RessourcesViewHolder> {
+
+    public static String PACKAGE_NAME;
 
     // Liste d'objets métier :
     private List<Ressource> listRessources = null;
@@ -30,12 +40,25 @@ public class RessourcesAdapter extends RecyclerView.Adapter<RessourcesViewHolder
     public RessourcesViewHolder onCreateViewHolder(ViewGroup parent, int ViewType) {
         View viewRessources = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_ressources, parent, false);
         return new RessourcesViewHolder(viewRessources);
+
+    }
+    public class activity extends Activity{
+        public void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+
+        }
+
     }
 
     @Override
     public void onBindViewHolder(RessourcesViewHolder ressourcesViewHolder, int position) {
         ressourcesViewHolder.getTextViewName().setText(listRessources.get(position).getName());
 
+        // Ne fonctionne pas car le ressource adapter n'est pas une activité :
+        //ressourcesViewHolder.getImageViewImage().setImageResource(getResources().getIdentifier("","drawable",getPackageName() ));
+
+        // Fonctionne mais retourne toujours la même image :
         ressourcesViewHolder.getImageViewImage().setImageResource(R.drawable.allemagnelogo);
 
         ressourcesViewHolder.getTextViewDescription().setText(listRessources.get(position).getDescription());
